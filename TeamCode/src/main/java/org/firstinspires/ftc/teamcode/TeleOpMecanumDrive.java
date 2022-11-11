4c233c70868711ebaf79b40e65afe84ea20a0429
FIXIT-3491 <97809407+FIXIT-3491@users.noreply.github.com>
2022-11-10 18:13:09 -0800

    Change program name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMecanumDrive.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
-public class AutoMecanumTeleOp extends LinearOpMode {
+public class TeleOpMecanumDrive extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
